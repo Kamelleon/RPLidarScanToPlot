44463d36111f649a6efe12029c4bc2d41e21fd4e
Kamil Bugla <kamil.bugla2002@gmail.com>
2021-05-18 13:54:29 +0200

    Add files via upload

---
+++ wawaw.py
@@ -0,0 +1,49 @@
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import pickle
+import plotly.graph_objs as go
+from collections import deque
+
+X = pickle.load(open("x_live.pkl", "rb"))
+
+Y = pickle.load(open("movement_live.pkl", "rb"))
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        dcc.Graph(id = 'live-graph'),
+        dcc.Interval(
+            id = 'graph-update',
+            interval = 1000,
+            n_intervals = 0
+        ),
+    ]
+)
+
+@app.callback(
+    Output('live-graph', 'figure'),
+    [ Input('graph-update', 'n_intervals') ]
+)
+
+def update_graph_scatter(n):
+    X = pickle.load(open("x_live.pkl", "rb"))
+    Y = pickle.load(open("movement_live.pkl", "rb"))
+
+    data = plotly.graph_objs.Scatter(
+            x=list(X),
+            y=list(Y),
+            marker_size=2,
+            name='Scatter',
+            mode= 'markers'
+    )
+
+    return {'data': [data],
+            'layout' : go.Layout(xaxis=dict(range=[min(X)-20,max(X)+20]),yaxis = dict(range = [-20,140]),)}
+
+if __name__ == '__main__':
+    app.run_server()
